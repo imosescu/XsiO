722ea1dbfdcbd2d60c0430cf7dec579925af2fa8
Tiberiu Mosescu <Tiberiu.Mosescu@worldpay.com>
2020-02-07 16:48:43 +0200

    correct file

--- runWithArgs.sh
+++ runWithArgs.sh
@@ -2,4 +2,4 @@
 # 1. Player is X
 # 2. Is a two player game
 # 3. Computer tries to lose
-java -jar target/XsiO.jar true false falsehttps://github.com/imosescu/XsiO.git
+java -jar target/XsiO.jar true false false
